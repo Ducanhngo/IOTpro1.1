0fc2c7025357db4430065838654b2bcf43ad53f8
Minh_Luong <minhluong10062006@gmail.com>
2025-02-25 15:47:26 +0700

    commit2

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'iotproject'
 
 login_manager = LoginManager(app)
